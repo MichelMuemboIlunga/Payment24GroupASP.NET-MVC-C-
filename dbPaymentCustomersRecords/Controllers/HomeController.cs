ee08c5903a3059db1100bde6698a27657987d4bd
michel muembo <michelmuemboilunga@gmail.com>
2021-07-11 15:06:23 +0200

    MVC payment24 project done

---
+++ dbPaymentCustomersRecords/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbPaymentCustomersRecords.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Customers()
+        {
+            dbpaymentsEntities dbpayments = new dbpaymentsEntities();
+
+            var data = dbpayments.prcShowProductSummaryWithRanks();
+            return View(data);
+        }
+    }
+}
